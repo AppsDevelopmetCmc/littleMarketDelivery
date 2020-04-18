1eadc1f451ca68b5e1b66afef78e7f89a35192ef
Santiago Mosquera <zantycb89@gmail.com>
2020-04-18 16:07:59 -0500

    cerrar sesión

--- app/screens/compras/ListaCompras.js
+++ app/screens/compras/ListaCompras.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet, Button } from "react-native";
+import * as firebase from "firebase";
 export class ListaCompras extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>LISTA DE COMPRAS</Text>
+        <Button
+          title="Cerrar Sesión"
+          onPress={() => {
+            firebase.auth().signOut();
+            console.log("Se cerro sesion");
+          }}
+        ></Button>
       </View>
     );
   }
