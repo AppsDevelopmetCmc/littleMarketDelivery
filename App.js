ad649c5c19ad7c68fc4978eb446975b60db66be2
Santiago Mosquera <zantycb89@gmail.com>
2020-04-18 13:57:15 -0500

    Navegacion y configuración firebase

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { cargarConfiguracion } from "./app/utils/FireBase";
+import NavegadorInicio from "./app/screens/navigation/NavegadorInicio";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  //cargarConfiguracion();
+  return <NavegadorInicio></NavegadorInicio>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/screens/navigation/NavegadorInicio.js
+++ app/screens/navigation/NavegadorInicio.js
@@ -1,79 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import * as firebase from 'firebase';
-import { View, Text } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
+import { View, Text } from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import PaginaInicio from '../PaginaInicio';
-import Registro from '../account/Registro';
-import IniciaSesion from '../account/IniciarSesion';
+import PaginaInicio from "../PaginaInicio";
+import Registro from "../account/Registro";
+import IniciaSesion from "../account/IniciarSesion";
+import { ListaCompras } from "../compras/ListaCompras";
+import { ListaPedidos } from "../pedidos/ListaPedidos";
 
-import MiCuenta from '../account/MiCuenta';
-import Mapa from '../map/Mapa';
-import Cargando from '../../components/Cargando';
+import MiCuenta from "../account/MiCuenta";
+import Mapa from "../map/Mapa";
+import Cargando from "../../components/Cargando";
+import { cargarConfiguracion } from "../../utils/FireBase";
 
 const StackAuthentication = createStackNavigator();
 const StackLogin = createStackNavigator();
+const TabHome = createBottomTabNavigator();
 
-const navOptionHandler = isValue => ({
-   headerShown: isValue,
+if (!global.firebaseRegistrado) {
+  cargarConfiguracion();
+}
+const navOptionHandler = (isValue) => ({
+  headerShown: isValue,
 });
 
 function AuthenticationStack() {
-   const [login, setLogin] = useState(null);
+  const [login, setLogin] = useState(null);
 
-   useEffect(() => {
-      firebase.auth().onAuthStateChanged(user => {
-         !user ? setLogin(false) : setLogin(true);
-      });
-   }, [login]);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      !user ? setLogin(false) : setLogin(true);
+    });
+  }, [login]);
 
-   if (login === null) {
-      return <Cargando isVisible={true} text="Cargando ..."></Cargando>;
-   } else {
-      return (
-         <StackAuthentication.Navigator>
-            {login ? (
-               <StackAuthentication.Screen
-                  name="Mapa"
-                  component={Mapa}
-               ></StackAuthentication.Screen>
-            ) : (
-               <StackAuthentication.Screen
-                  name="LoginStack"
-                  component={LoginStack}
-                  options={navOptionHandler(false)}
-               ></StackAuthentication.Screen>
-            )}
-         </StackAuthentication.Navigator>
-      );
-   }
+  if (login === null) {
+    return <Cargando isVisible={true} text="Cargando ..."></Cargando>;
+  } else {
+    return (
+      <StackAuthentication.Navigator>
+        {login ? (
+          <StackAuthentication.Screen
+            name="HomeTab"
+            component={HomeTab}
+            options={navOptionHandler(false)}
+          ></StackAuthentication.Screen>
+        ) : (
+          <StackAuthentication.Screen
+            name="LoginStack"
+            component={LoginStack}
+            options={navOptionHandler(false)}
+          ></StackAuthentication.Screen>
+        )}
+      </StackAuthentication.Navigator>
+    );
+  }
 }
 
+function HomeTab() {
+  return (
+    <TabHome.Navigator>
+      <TabHome.Screen
+        name="ListaCompras"
+        component={ListaCompras}
+      ></TabHome.Screen>
+      <TabHome.Screen
+        name="ListaPedidos"
+        component={ListaPedidos}
+      ></TabHome.Screen>
+    </TabHome.Navigator>
+  );
+}
 function LoginStack() {
-   return (
-      <StackLogin.Navigator>
-         <StackLogin.Screen
-            name="Pagina Inicio"
-            component={PaginaInicio}
-            options={navOptionHandler(false)}
-         ></StackLogin.Screen>
-         <StackLogin.Screen
-            name="Registro"
-            component={Registro}
-         ></StackLogin.Screen>
-         <StackLogin.Screen
-            name="IniciaSesion"
-            component={IniciaSesion}
-         ></StackLogin.Screen>
-      </StackLogin.Navigator>
-   );
+  return (
+    <StackLogin.Navigator>
+      <StackLogin.Screen
+        name="Pagina Inicio"
+        component={PaginaInicio}
+        options={navOptionHandler(false)}
+      ></StackLogin.Screen>
+      <StackLogin.Screen
+        name="Registro"
+        component={Registro}
+      ></StackLogin.Screen>
+      <StackLogin.Screen
+        name="IniciaSesion"
+        component={IniciaSesion}
+      ></StackLogin.Screen>
+    </StackLogin.Navigator>
+  );
 }
 
 export default function NavegadorInicio() {
-   return (
-      <NavigationContainer>
-         <AuthenticationStack></AuthenticationStack>
-      </NavigationContainer>
-   );
+  return (
+    <NavigationContainer>
+      <AuthenticationStack></AuthenticationStack>
+    </NavigationContainer>
+  );
 }
