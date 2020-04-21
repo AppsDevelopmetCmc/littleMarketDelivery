b770578a4437431c85cddeaddcea7d110817abe9
carolina <carolia.hurtado@clearminds-it.com>
2020-04-20 19:47:50 -0500

    AHU-archivo crud firebase

--- app/utils/FireBase.js
+++ app/utils/FireBase.js
@@ -1,5 +1,12 @@
 import firebase from "firebase";
 import "@firebase/firestore";
+import { decode, encode } from 'base-64'
+global.crypto = require("@firebase/firestore");
+global.crypto.getRandomValues = byteArray => { for (let i = 0; i < byteArray.length; i++) { byteArray[i] = Math.floor(256 * Math.random()); } }
+
+if (!global.btoa) { global.btoa = encode; }
+
+if (!global.atob) { global.atob = decode; }
 
 export const cargarConfiguracion = () => {
   const firebaseConfig = {
@@ -12,5 +19,6 @@ export const cargarConfiguracion = () => {
     appId: "1:549900659572:web:ce8621915b320376469a21",
   };
   firebaseApp = firebase.initializeApp(firebaseConfig);
+  global.db = firebase.firestore();
   global.firebaseRegistrado = true;
 };
